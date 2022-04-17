b238b9c85d38f5219a9adfb03858fdf8850f181b
Rohan Verma <rohanvermaara@gmail.com>
2022-04-17 21:43:36 +0530

    updated path resolution to support linux

--- router/view.js
+++ router/view.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path  = require('path')
 const router = express.Router()
 
-const UI_PATH = path.resolve(__dirname, '../view')+'\\'
+const UI_PATH = path.resolve(__dirname, '../view')+'/'
 
 router.get('/', (req, res) => {
     res.sendFile(`${UI_PATH}index.html`)
